153a62cadf0cdce1007e0705a3a38e48608cc589
Blessing ingabire <b.ingabire1@alustudent.com>
2026-09-24 14:19:18 +0200

    Refactor variational autoencoder implementation

--- unsupervised_learning/autoencoders/3-variational.py
+++ unsupervised_learning/autoencoders/3-variational.py
@@ -3,112 +3,73 @@
 
 import tensorflow.keras as keras
 
+
 def autoencoder(input_dims, hidden_layers, latent_dims):
-"""
-Creates a variational autoencoder.
+    """
+    Creates a variational autoencoder.
 
-```
-Args:
-    input_dims: dimensions of the model input
-    hidden_layers: list containing the number of nodes for each
-        hidden layer in the encoder
-    latent_dims: dimensions of the latent space representation
+    Returns:
+        encoder: Encoder model
+        decoder: Decoder model
+        auto: Full autoencoder model
+    """
 
-Returns:
-    encoder, decoder, auto
-"""
-X = keras.Input(shape=(input_dims,))
+    # Encoder
+    encoder_input = keras.Input(shape=(input_dims,))
 
-Y = X
-for units in hidden_layers:
-    Y = keras.layers.Dense(
-        units,
-        activation='relu'
-    )(Y)
+    x = encoder_input
+    for nodes in hidden_layers:
+        x = keras.layers.Dense(nodes, activation="relu")(x)
 
-z_mean = keras.layers.Dense(
-    latent_dims,
-    activation=None
-)(Y)
+    # Mean and log variance
+    z_mean = keras.layers.Dense(latent_dims, activation=None)(x)
+    z_log_var = keras.layers.Dense(latent_dims, activation=None)(x)
 
-z_log_sigma = keras.layers.Dense(
-    latent_dims,
-    activation=None
-)(Y)
+    # Sampling layer
+    def sampling(args):
+        z_mean, z_log_var = args
+        epsilon = keras.backend.random_normal(
+            shape=keras.backend.shape(z_mean)
+        )
+        return z_mean + keras.backend.exp(0.5 * z_log_var) * epsilon
 
-def sampling(args):
-    """Samples a point from the latent distribution."""
-    z_mean, z_log_sigma = args
-    epsilon = keras.backend.random_normal(
-        shape=keras.backend.shape(z_mean)
-    )
-    return z_mean + keras.backend.exp(
-        z_log_sigma / 2
-    ) * epsilon
+    z = keras.layers.Lambda(sampling)([z_mean, z_log_var])
 
-z = keras.layers.Lambda(
-    sampling,
-    output_shape=(latent_dims,)
-)([z_mean, z_log_sigma])
-
-encoder = keras.Model(
-    X,
-    [z, z_mean, z_log_sigma]
-)
-
-X_decoder = keras.Input(
-    shape=(latent_dims,)
-)
-
-Y = X_decoder
-for units in reversed(hidden_layers):
-    Y = keras.layers.Dense(
-        units,
-        activation='relu'
-    )(Y)
-
-Y = keras.layers.Dense(
-    input_dims,
-    activation='sigmoid'
-)(Y)
-
-decoder = keras.Model(
-    X_decoder,
-    Y
-)
-
-auto_output = decoder(z)
-
-auto = keras.Model(
-    X,
-    auto_output
-)
-
-def vae_loss(x, x_decoder_mean):
-    """Calculates the variational autoencoder loss."""
-    reconstruction_loss = keras.backend.binary_crossentropy(
-        x,
-        x_decoder_mean
-    )
-    reconstruction_loss = keras.backend.sum(
-        reconstruction_loss,
-        axis=1
+    encoder = keras.Model(
+        encoder_input,
+        [z, z_mean, z_log_var],
+        name="encoder"
     )
 
-    kl_loss = -0.5 * keras.backend.sum(
-        1
-        + z_log_sigma
-        - keras.backend.square(z_mean)
-        - keras.backend.exp(z_log_sigma),
-        axis=1
+    # Decoder
+    decoder_input = keras.Input(shape=(latent_dims,))
+
+    x = decoder_input
+    for nodes in reversed(hidden_layers):
+        x = keras.layers.Dense(nodes, activation="relu")(x)
+
+    decoder_output = keras.layers.Dense(
+        input_dims,
+        activation="sigmoid"
+    )(x)
+
+    decoder = keras.Model(
+        decoder_input,
+        decoder_output,
+        name="decoder"
     )
 
-    return reconstruction_loss + kl_loss
+    # Full autoencoder
+    auto_output = decoder(z)
+    auto = keras.Model(
+        encoder_input,
+        auto_output,
+        name="autoencoder"
+    )
 
-auto.compile(
-    loss=vae_loss,
-    optimizer='adam'
-)
+    auto.compile(
+        optimizer="adam",
+        loss="binary_crossentropy"
+    )
 
-return encoder, decoder, auto
-```
+    return encoder, decoder, auto
